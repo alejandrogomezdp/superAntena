cccc0ec095ccbfc48657bd85063bd476a37636db
alejandrogomezdp <alejandrogomezdp>
2023-06-26 16:32:21 +0200

    after product list with cart

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,30 +1,46 @@
+import React, { useState } from 'react';
 import dataProductos from './Db/db_calc';
 import './Calculadora.css'; 
+import { FaShoppingCart } from 'react-icons/fa';
 
 const ComponenteCalculadora = () => {
+  const [carrito, setCarrito] = useState([]);
+
+  const añadirAlCarrito = (producto) => {
+    setCarrito([...carrito, producto]);
+  };
+
+  const calcularTotal = () => {
+    return carrito.reduce((acc, producto) => acc + producto.Precio, 0);
+  };
 
   return (
     <div className="container">
-      {dataProductos.map(product => (
-        <div className="nested" key={product.Id}>
+      <div className="carrito">
+        <FaShoppingCart size="1.5em" />
+        <span>{carrito.length}</span>
+        <span>{calcularTotal()}€</span>
+      </div>
+      {dataProductos.map(producto => (
+        <div className="nested" key={producto.Id}>
           <div className="text-right line-height">
             <div className="box">
-              <div className="header-text">{product.Id}</div>
+              <div className="header-text">{producto.Id}</div>
               <div className="nested">
                 <div className="flex-container">
                   <div className="image-container">
-                    <img className="image" src={product.Img} alt="" />
+                    <img className="image" src={producto.Img} alt=""></img>
                   </div>
                   <div className="price">
-                    <span className="price-big">{product.Precio}€</span>
-                    <span className="price-small">{product.PrecioSinIva}€ s/iva</span>
-                    <span className="delivery-info">24 horas</span>   
+                    <span className="price-big">{producto.Precio}€</span>
+                    <span className="price-small">{producto.PrecioSinIva}€ s/iva</span>
+                    <span className="delivery-info">24 horas</span>
                   </div>
                   <div className="form">
                     <div className="table">
                       <div className="input-container">
-                        <input type="number" name={`cantidad[${product.Id}]`} maxLength="12" min="1" defaultValue="1" title="Cantidad" id={`ControlID-${product.Id}`} className="quantity" />
-                        <button type="button" title="Añadir a la Cesta" id={`ControlID-${product.Id + 1}`} className="button">Añadir a la Cesta</button>
+                        <input type="number" name="cantidad[]" maxLength="12" min="1" defaultValue="1" title="Cantidad" className="quantity"></input>
+                        <button type="button" title="Añadir a la Cesta" className="button" onClick={() => añadirAlCarrito(producto)}>Añadir a la Cesta</button>
                       </div>
                     </div>
                   </div>
--- src/Db/db_calc.js
+++ src/Db/db_calc.js
@@ -4,13 +4,13 @@ const dataProductos = [
         "Id": 1,
         "Img": ("https://www.ilumitec.es/imagenes/mini/marco-blanco-blanco-simon-82.jpg"),
         "Precio": 25733,
-        "PrecioSinIva": "1LwPvnzvENhr2KszJNdX6ffwWiAEDCStSR"
+        "PrecioSinIva": "2573"
     },
     {
         "Id": 2,
         "Img": ("https://www.ilumitec.es/imagenes/mini/marco-blanco-blanco-simon-82.jpg"),
         "Precio": 63793,
-        "PrecioSinIva": "1s8VTHERHd55puhidYTh5nEQwvBLeVXrM"
+        "PrecioSinIva": "6379"
     },
 
 ];
